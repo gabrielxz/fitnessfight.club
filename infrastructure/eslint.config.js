481dcb4edb388513abf09d6507a6ac47963cb020
Gabriel Beal <gabrielbeal@gmail.com>
2025-08-19 17:42:51 -0400

    fix: add missing config files for ESLint and Jest

---
+++ infrastructure/eslint.config.js
@@ -0,0 +1,34 @@
+const tseslint = require('@typescript-eslint/eslint-plugin');
+const tsParser = require('@typescript-eslint/parser');
+
+module.exports = [
+  {
+    files: ['**/*.ts'],
+    languageOptions: {
+      parser: tsParser,
+      parserOptions: {
+        project: './tsconfig.json',
+        tsconfigRootDir: __dirname,
+      },
+    },
+    plugins: {
+      '@typescript-eslint': tseslint,
+    },
+    rules: {
+      ...tseslint.configs.recommended.rules,
+      'no-new': 'off',
+      '@typescript-eslint/no-unused-vars': [
+        'error',
+        {
+          argsIgnorePattern: '^_',
+          varsIgnorePattern: '^_',
+        },
+      ],
+      '@typescript-eslint/no-explicit-any': 'warn',
+      'no-console': ['warn', { allow: ['warn', 'error'] }],
+    },
+  },
+  {
+    ignores: ['node_modules/', 'dist/', 'cdk.out/', '*.js', '*.d.ts', 'jest.config.js'],
+  },
+];
